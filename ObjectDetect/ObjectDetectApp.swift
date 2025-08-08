36b1c51a3a11d38efdc35fe8f84da0615befb2b6
aayush4vedi <4vedi.aayush@gmail.com>
2025-08-08 19:17:13 +0530

    Initial Commit

---
+++ ObjectDetect/ObjectDetectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObjectDetectApp.swift
+//  ObjectDetect
+//
+//  Created by Aayush Chaturvedi on 08/08/25.
+//
+
+import SwiftUI
+
+@main
+struct ObjectDetectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ObjectDetectTests/ObjectDetectTests.swift
@@ -0,0 +1,16 @@
+//
+//  ObjectDetectTests.swift
+//  ObjectDetectTests
+//
+//  Created by Aayush Chaturvedi on 08/08/25.
+//
+
+import Testing
+
+struct ObjectDetectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
